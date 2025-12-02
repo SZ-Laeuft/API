6e72f277c72da2f20efb16fd68d13a1b68ea1fd8
fabxx45 <167181241+fabxx45@users.noreply.github.com>
2025-12-02 14:15:26 +0100

    change to different ip address

--- SZL_Backend/SZL_Backend/Context/SZLDbContext.cs
+++ SZL_Backend/SZL_Backend/Context/SZLDbContext.cs
@@ -36,7 +36,7 @@ public partial class SZLDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=100.102.196.30;Port=5432;Database=szl;Username=admin;Password=Szl-20010901;");
+        => optionsBuilder.UseNpgsql("Host=100.94.128.1;Port=5432;Database=szl;Username=admin;Password=Szl-20010901;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
